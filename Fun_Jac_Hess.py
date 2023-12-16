957d8d59c4eb4ebed6f7c27b166fe78aebc8584a
Gianmichele <palumbo.gianmichele@gmail.com>
2023-12-16 16:46:33 +0100

    Tried with new functions

--- Fun_Jac_Hess.py
+++ Fun_Jac_Hess.py
@@ -1,36 +1,4 @@
 import numpy as np
-from scipy.optimize import minimize
-from sympy import symbols, lambdify
-
-image_size = 28  # width and length
-no_of_different_labels = 10  #  i.e. 0, 1, 2, 3, ..., 9
-image_pixels = image_size * image_size
-data_path = "data/mnist/"
-train_data = np.loadtxt(data_path + "mnist_train.csv",
-                        delimiter=",")
-test_data = np.loadtxt(data_path + "mnist_test.csv",
-                       delimiter=",")
-
-# Extract 0s from the training set
-A = train_data[train_data[:, 0] == 0.]
-
-# Delete the first column
-A = A[0:5, 1:]
-
-# Only keep some columns such that first element is non zero (for start 10 of them)
-non_zero_ind = np.nonzero(A[0, :])[0]
-indices = np.random.choice(non_zero_ind, size=10, replace=False)
-A = A[:, indices]
-
-# Extract non-0s
-B = train_data[train_data[:, 0] != 0.]
-# Delete first column
-B = B[0:5, 1:]
-# Keep the same columns you kept in A
-B = B[:, indices]
-
-
-
 def Barrier(x,lambd,A,B):
     '''A,B are the matrices defining parameters of G
     x is an 1D-array of lenght n+n_a+n_b+2, x = [h,c,p,s,t]'''
@@ -44,11 +12,11 @@ def Barrier(x,lambd,A,B):
     n_b = len(B)
 
     # Extract variables
-    h = x0[:n]
-    c = x0[n]
-    p = x0[n + 1]
-    s = x0[n + 2:n + 2 + n_a]
-    t = x0[n + 2 + n_a:]
+    h = x[:n]
+    c = x[n]
+    p = x[n + 1]
+    s = x[n + 2:n + 2 + n_a]
+    t = x[n + 2 + n_a:]
 
     # Initialize the value of barrier at 0
     G = 0
@@ -140,7 +108,7 @@ def Jacobian(x,mu,lambd,A,B):
     J = np.array(J)
     return J
 
-def Hessian(x,mu,lambd,A,B):
+def Hessian(x,lambd,A,B):
     '''Evaluates the Hessian of the new_objective'''
 
     # Number of features
@@ -281,6 +249,7 @@ def Hessian(x,mu,lambd,A,B):
     H = np.array(H)
     return H
 
+'''
 # Number of features
 n = len(A[0, :])
 # Number of points in A
@@ -307,5 +276,6 @@ print(f'Jac = {Jac}')
 
 Hes = Hessian(x0,mu,lambd,A,B)
 print(f'Hes = {Hes}')
+'''
 
 
---
+++ SPFM.py
@@ -0,0 +1,139 @@
+import numpy as np
+import Fun_Jac_Hess as fun
+
+def Read_Data(n,n_a,n_b):
+    image_size = 28  # width and length
+    no_of_different_labels = 10  # i.e. 0, 1, 2, 3, ..., 9
+    image_pixels = image_size * image_size
+    data_path = "data/mnist/"
+    train_data = np.loadtxt(data_path + "mnist_train.csv",
+                            delimiter=",")
+    test_data = np.loadtxt(data_path + "mnist_test.csv",
+                           delimiter=",")
+
+    # Extract 0s from the training set
+    A = train_data[train_data[:, 0] == 0.]
+
+    # Delete the first column
+    A = A[0:n_a, 1:]
+
+    # Only keep some columns such that first element is non zero (for start 10 of them)
+    non_zero_ind = np.nonzero(A[0, :])[0]
+    indices = np.random.choice(non_zero_ind, size=n, replace=False)
+    A = A[:, indices]
+
+    # Extract non-0s
+    B = train_data[train_data[:, 0] != 0.]
+    # Delete first column
+    B = B[0:n_b, 1:]
+    # Keep the same columns you kept in A
+    B = B[:, indices]
+
+    return A,B
+
+def short_path_method(A,B,lambd = 5, eps = 1e-3):
+    '''This function implements the short path following method to optimize
+    the problem in Homework 3 of the course Optimization Models and methods II
+    2023/2024
+    Input:  lambd: regularization coefficient
+            A: set 1, matrix with coordinates of points as rows
+            B: set 2, same as A
+    '''
+
+    # Number of features
+    n = len(A[0, :])
+    # Number of points in A
+    n_a = len(A)
+    # Number of points in B
+    n_b = len(B)
+
+
+    # Initialize with x_0 and mu_0
+    x_0, mu_0 = initialization(F, n, n_a, n_b)
+
+    # Choose tau
+    tau = .25
+
+    #Choose theta # need a way to evaluate mu
+    theta = (16*np.sqrt(v))**(-1)
+
+    # Compute mu_final
+    mu_f = eps*(1-tau)/v
+
+    mu = mu_0
+    x = x_0
+    # While cycle
+    while mu > mu_f:
+
+        # Update mu
+        mu = (1-theta)*mu_
+
+        # Evaluate Gradient
+        J = Jacobian(x,mu,lambd,A,B)
+
+        # Evalutate Hessian
+        H = Hessian(x,mu,lambd,A,B)
+
+        # Newton step
+        #x = need to define newton step
+
+    return x
+
+def initialization(A,B,lambd):
+    '''First draft: chose an interior point x_0 and find a mu_0 such that
+    delta_mu_0 < 1'''
+
+    # Number of features
+    n = len(A[0, :])
+    # Number of points in A
+    n_a = len(A)
+    # Number of points in B
+    n_b = len(B)
+
+    # Choose mu_0
+    mu_0 = 1
+
+    x0 = np.array([0 for i in range(n)] + [0] + [5] + [2 for i in range(n_a)] + [2 for i in range(n_b)])
+
+    # Initialize delta
+    delta = 1
+    # Get closer to path until delta < 1
+    while delta >= 1:
+        # Do a Damped Newton step to decrease delta
+        x0, delta = damped_N(x0,A,B,lambd,mu_0)
+
+    return x0, mu_0
+
+def damped_N(x0,A,B,lambd,mu=1):
+    '''Evaluate 1 damped newton step, in particular:
+    if delta less then 1 return x0 and delta
+    else perform a damped newton step and return new x and 1'''
+
+    # Evaluate gradient at x0
+    J = fun.Jacobian(x0,mu,lambd,A,B)
+
+    # Evaluate Hessian at x0
+    H = fun.Hessian(x0,lambd,A,B)
+    print(np.linalg.eigvals(H))
+
+    # Evaluate newton step
+    newton= np.linalg.solve(H, J)
+
+    # Evaluate delta
+    #    print(G)
+    #    print(np.dot(G, n))
+    delta = np.sqrt(np.dot(J, newton))
+    print(delta)
+
+    # Break if delta is less then 1
+    if delta < 1:
+        return x0, delta
+
+    # Evaluate new x
+    x = x0 - newton/(1+delta)
+
+    # Attentiom: returned delta is the one associated to x0, not to x
+    return x, delta
+
+A,B = Read_Data(10,5,5)
+x_0,mu_0 = initialization(A,B,lambd=5)
